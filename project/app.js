d1a5f7e9214ed0bff255a0653cd6f913ecf88b9e
Administrator <so@AF.fritz.box>
2023-10-18 20:35:35 +0200

    updates 1

--- project/app.js
+++ project/app.js
@@ -1,18 +1,24 @@
-
-function onScanSuccess(qrCodeMessage) {
-        alert(`QR Code detected: ${qrCodeMessage}`);
-    }
-
-const html5QrCode = new Html5Qrcode("qr-video");
-
+// Function to start QR code scanning
 function startScanning() {
-    html5QrCode.start(
-        {
-            fps: 10,
-            qrbox: 250,
-        },
-        onScanSuccess
-    );
+    // Create a new Instascan scanner
+    const scanner = new Instascan.Scanner({ video: document.getElementById('qr-video') });
+
+    // Add a listener for the 'scan' event
+    scanner.addListener('scan', function (content) {
+        alert('QR Code detected: ' + content);
+    });
+
+    // Start the scanner
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
 }
 
+// Call the function to start scanning
 startScanning();
